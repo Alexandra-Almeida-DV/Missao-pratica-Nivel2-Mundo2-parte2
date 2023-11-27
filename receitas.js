7521137e727f7af547ba315291aebcecb13549a0
Alexandra-Almeida-DV <138154188+Alexandra-Almeida-DV@users.noreply.github.com>
2023-11-26 22:36:30 -0300

    Add files via upload

---
+++ receitas.js
@@ -0,0 +1,19 @@
+[{
+    "titúlo": "ARROZ DE FORNO",
+    "imagem": "./img/Arroz de forno.jpeg",
+    "ingredientes": "4 xícaras (chá) de arroz cozido",
+    "preparo": "Em uma tigela misture o arroz, a mussarela, o presunto",
+},
+{
+    "titúlo": "FRANGO XADREZ",
+    "imagem": "./img/frango xadrez.jpeg",
+    "ingredientes": "5 files de peito de frango",
+    "preparo": "Tempere o frango com sal e pimenta e refogue-o no óleo.",
+},
+{
+    "titúlo": "GELADO DE ABACAXI",
+    "imagem": "./img/gelado de abacaxi.jpeg",
+    "ingredientes": "1 abacaxi médio cortado em cubos",
+    "preparo": "Em uma panela coloque o abacaxi picado em cubos, a água",
+}
+]
